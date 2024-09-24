67094deb35121222f5e6f1397efe7356e3045586
aniket19022003 <aniket19022003@gmail.com>
2024-09-24 04:39:59 +0000

    added-more-projects

--- my-web-profile/src/pages/Projects.js
+++ my-web-profile/src/pages/Projects.js
@@ -4,7 +4,7 @@ import './Projects.css';
 const projects = [
   {
     title: "Portfolio Website",
-    description: "A personal portfolio website built with React.",
+    description: "A personal portfolio website built with React just as a web-dev practice and hands-on experience",
     techStack: "React, JavaScript, CSS, HTML",
     link: "https://github.com/AniketKumar10/my-web-profile",
     // imgSrc: "path-to-image", // Optional project image
@@ -16,6 +16,19 @@ const projects = [
     link: "https://github.com/AniketKumar10/medicine-reccomendation-better-ui",
     //imgSrc: "path-to-image", // Optional project image
   },
+  {
+    title: "Trip Cost Estimator",
+    description : "From the dataset extracted, an BI page was generated for the user(org.) to view data better in graphical ways",
+    techStack: "AWS, GCP, LookerStudio, Python, Jupyternotebook",
+    link: "https://lookerstudio.google.com/s/tlJl_3jgv6I",
+    //imgSrc: "path-to-image", // Optional project image
+  },
+  {
+    title:"DeadLine Detector",
+    description:"The project was build based on an OCR system and ML/AI that reads documents and creates essential reminders accordingly",
+    techStack:"NanoNets, ML, AI",
+    link:""
+  }
   // Add more projects
 ];
 
